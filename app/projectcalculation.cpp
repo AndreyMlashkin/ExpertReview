8cac14e69cda402dbd821e17681d319b60ab6c71
AndreyMlashkin <malashkin.andrey@mail.ru>
2015-10-15 16:45:28 +0300

    formuls corrected strings 84 85 in constants added

--- app/projectcalculation.cpp
+++ app/projectcalculation.cpp
@@ -88,48 +88,51 @@ QList<double> calculateProject(QList<double> _source)
     ans[7]  =(_source[18] + _source[19] + _source[20]) /
              (_source[21] + _source[22] + _source[23]) ;
     ans[8]  = _source[24] / _source[25];
-    ans[10] = _source[26] / _source[27];
-    ans[11] = _source[28] / _source[29];
-    ans[12] = _source[30] / _source[31];
-    ans[13] = _source[32] / _source[33];
-    ans[14] = _source[34] / _source[35];
-    ans[15] = _source[36] / _source[37];
-    ans[16] = _source[38] / _source[39];
-    ans[17] = _source[40] * _source[41] / _source[42];
-    ans[18] = _source[43] + _source[44] + _source[45];
-    ans[19] =  1.0        - _source[46] / _source[47];
-    ans[20] = _source[48] + _source[49] + _source[50];
-    ans[21] =(_source[51] + _source[52] + _source[53])/ _source[54];
-    ans[22] = _source[55] + _source[56] + _source[57] + _source[58];
-    ans[23] = _source[59] * _source[60] * _source[61];
-    ans[24] = _source[62] / _source[63];
-    ans[25] = _source[64] / _source[65];
-    ans[26] = _source[66] + _source[67] + _source[68] + _source[69];
-    ans[27] = _source[70] + _source[71] + _source[72];
-    ans[28] = 1140 * _source[74] / _source[73] +
-             (_source[75] * _source[76] * _source[77]) / _source[78]; // train + auto
-    ans[29] = _source[79] / _source[80];
-    ans[30] = _source[81] / _source[82];
-    ans[31] = _source[83] / _source[84];
-    ans[32] = _source[85] + _source[86] + _source[87] + _source[88];
-    ans[33] = _source[89] + _source[90] + _source[91];
-    ans[34] = _source[92] / _source[93] + (_source[94] * _source[95]) / _source[96];//
-    ans[35] = _source[97] / _source[98];
+    ans[9]  = _source[26] / _source[27]; // эта строчка была добавлена
+    ans[10]	= _source[28] / _source[29];
+    ans[11]	= _source[30] / _source[31];
+    ans[12]	= _source[32] / _source[33];
+    ans[13]	= _source[34] / _source[35];
+    ans[14]	= _source[36] / _source[37];
+    ans[15]	= _source[38] / _source[39];
+    ans[16]	= _source[40] * _source[41] / _source[42]; // Количество создаваемых (сохраняемых) рабочих мест  в смежных отраслях
+    ans[17]	= _source[43] + _source[44] + _source[45];
+    ans[18]	=  1       -    _source[46] / _source[47]; // Наличие трудовых ресурсов соответствующей квалификации
+    ans[19]	= _source[48] + _source[49] + _source[50];
+    ans[20]	=(_source[51] + _source[52] + _source[53])/ _source[54];
+    ans[21]	= _source[55] + _source[56] + _source[57] + _source[58];
+    ans[22]	= _source[59] * _source[60] * _source[61]; // Сокращение времени пребывания пассажиров в пути (увеличение агломерационного радиуса)
+    ans[23]	= _source[62] / _source[63];
+    ans[24]	= _source[64] / _source[65];
+    ans[25]	= _source[66] + _source[67] + _source[68] + _source[69];
+    ans[26]	= _source[70] + _source[71] + _source[72];
+    ans[27]	= 1140 * _source[74] / _source[73] +
+             (_source[75] * _source[76] * _source[77]) / _source[78]; // train + auto. Увеличение пропускной способности путей сообщения
+    ans[28]	= _source[79] / _source[80];
+    ans[29] = _source[81] / _source[82];
+    ans[30]	= _source[83] / _source[84]; // Платежная способность населения в регионе
+    ans[31]	= _source[85] / _source[86];
+    ans[32]	= _source[87] + _source[88] + _source[89] + _source[90];
+    ans[33]	= _source[91] + _source[92] + _source[93];
+    ans[34]	= _source[94] / _source[95] + (_source[96] * _source[97]) / _source[98];//
+    ans[35]	= _source[99] / _source[100]; // Изменение населенности подвижного состава
 
-//    for(int i = 98; i < _source.size(); ++i)
-//        ans << _source.at(i);
 
     for(int i = 36; i <= 57; i++)
     {
        ans[i] = _source[i + 63];
     }
 
-    double errorFlag = 0.0d / 0.0d;
     for(int i = 0; i < ans.size(); ++i)
     {
-        if(ans[i] == errorFlag)
+        if(ans[i] == std::isnan(ans[i]) || ans[i] == std::isinf(ans[i]))
+        {
+            qDebug() << "WARNING! ans[" << i << "] is nan!!!";
             ans[i] = 0;
+        }
     }
+
+
 
     return ans.toList();
 }
